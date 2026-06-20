59298fe9c11ff23b5ec50c032c6904398efb4976
Danixz <danielmobile098@gmail.com>
2026-06-19 23:24:07 -0300

    Novo curriculo

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,7 +1,7 @@
 document.getElementById("download-btn").addEventListener("click", function() {
     // Criar um link temporário
     let link = document.createElement("a");
-    link.href = "/src/CV/CV-Daniel Friedrich de Moura.pdf";
+    link.href = "/src/CV/Daniel Friedrich de Moura.pdf";
     link.download = "Daniel Friedrich de Moura - CV.pdf"; // Nome do arquivo ao baixar
     document.body.appendChild(link);
     link.click();
